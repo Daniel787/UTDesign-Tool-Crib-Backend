6ad591956dd39f26ad6e0c70c80c2272b378df36
sudhijag <37680391+sudhijag@users.noreply.github.com>
2021-03-21 09:27:34 -0500

    new query style, safer against injection attack

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,6 +1,7 @@
 var express = require('express');
+const nodemon = require('nodemon');
 var router = express.Router();
-var bodyParser = require('body-parser')
+//var bodyParser = require('body-parser')
 
 //sql connection
 var pool = require('../db.js')
@@ -24,8 +25,8 @@ router.get("/search",(req,res) => {
   //arguments
   var id= req.query.id
 
-  myquery= "SELECT * FROM mydb.inventory_part WHERE part_id="+id
-    pool.query(myquery, function (err, rows, fields) {
+  myquery= "SELECT * FROM mydb.inventory_part WHERE part_id=?"
+    pool.query(myquery, [id], function (err, rows, fields) {
       if (err) throw err
   
       console.log('The solution is: ', rows)
@@ -35,14 +36,15 @@ router.get("/search",(req,res) => {
     console.log('finished route')
 });
 
-//i.e. http://localhost:3006/inventory/search?name=phil 
+//i.e. http://localhost:3006/inventory/searchname?name=phil 
 //will theoretically return 12345 phillips screw x
 router.get("/searchname",(req,res) => {
   //arguments
   var name= req.query.name
 
-    myquery= "SELECT * FROM mydb.inventory_part WHERE LOWER(name) LIKE LOWER('%"+ name+ "%')"
-    pool.query(myquery, function (err, rows, fields) {
+    //myquery= "SELECT * FROM mydb.inventory_part WHERE LOWER(name) LIKE LOWER(%" + name+ "%)"
+    myquery= "SELECT * FROM mydb.inventory_part WHERE LOWER(name) LIKE LOWER(CONCAT('%', ?, '%'))"
+    pool.query(myquery, [name], function (err, rows, fields) {
       if (err) throw err
   
       console.log('The solution is: ', rows)
@@ -83,26 +85,35 @@ router.post("/insert",(req,res) => {
   //later: test if any of the parameters are null
 
   //myquery= "insert into mydb.inventory_part (part_id, name, quantity_available, current_cost) VALUES (11115, 'Hot Iron', 11, .55)"
-  for(i=0; i < req.cart.length; i++){
-    var transcation_id= 556; //this is just some random number for now
+  console.log(req.body);
+  
+  for(i=0; i < req.body.cart.length; i++){
+  //for(i=0; i < 1; i++){
+    var transaction_id= 556; //this is just some random number for now
     var part_id= req.body.cart[i].item.part_id
-    var net_id=NULL;
-    var date= NULL;
-    var description= NULL;
+    var group_id=357;
+    var type= "rent"
+    var net_id=req.body.net_id;
+    //var date= today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var date= "2020-11-11";
+    var description= "OK";
     var name= req.body.cart[i].item.name
-    var quantity_wanted= req.body.cart[i].item.quantity
+    var quantity_purchased= req.body.cart[i].quantity
     var current_cost= req.body.cart[i].item.current_cost
     
-    myquery= "INSERT into mydb.purchase (transcation_id, part_id, net_id, date, description, name, quantity_available, current_cost) VALUES (" + transaction_id + ", " + part_id+ ", '" + net_id+ ", '"+  date +"' , '"+ description + "' , '"+ name + "' , " + quantity_wanted + ", " + current_cost + ")"
-    pool.query(myquery, function (err, rows, fields) {
+    console.log("OK")
+    //myquery= "INSERT into mydb.transaction (transaction_id, part_id, quantity_purchased, purchased_cost) VALUES (" + "556" + ", " + "12345" + ", "+  "6" + current_cost + ");" +  "INSERT into mydb.purchased_part (transaction_id, part_id, quantity_purchased, purchased_cost) VALUES (" + transaction_id + ", " + "12345" + ", " + quantity_purchased + ", " + current_cost + ")"
+    myquery= "INSERT INTO mydb.transaction(transaction_id, group_id, net_id, date, type) VALUES (?, ?, ?, ?, ?); INSERT into mydb.purchased_part (transaction_id, part_id, quantity_purchased, purchased_cost) VALUES(?, ?, ?, ?)"
+    pool.query(myquery, [ transaction_id, group_id, net_id, date, type, transaction_id, part_id, quantity_purchased, current_cost], function (err, rows, fields) {
         if (err) throw err
         else{
           res.json("Successfully inserted.");
         }
       })
-
+   
       console.log('finished route')
   }
+
 });
 
 
