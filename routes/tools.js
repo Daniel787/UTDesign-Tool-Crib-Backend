01f8d256ef80b55ae4e1c0c70cf81da39d703a8e
Daniel Lopez <dlopezvelazquez@live.com>
2021-03-28 21:49:10 -0500

    changed tool db.js from pool to connection fix

--- routes/tools.js
+++ routes/tools.js
@@ -1,48 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 //sql connection
-var pool = require('../db.js')
+var connection = require("../db.js");
 
-router.get("/",(req,res) => {
-
-  myquery= "SELECT * FROM mydb.rental_tool"
+router.get("/", (req, res) => {
+  myquery = "SELECT * FROM mydb.rental_tool";
   connection.query(myquery, function (err, rows, fields) {
-    if (err) throw err
+    if (err) throw err;
     res.json(rows);
-  })
+  });
 
-  console.log('finished route')
+  console.log("finished route");
 });
 
 //i.e. http://localhost:port/tools/search?id=111
-router.get("/search",(req,res) => {
+router.get("/search", (req, res) => {
   //arguments
-  var id= req.query.id
+  var id = req.query.id;
 
-  myquery= "SELECT * FROM mydb.rental_tool WHERE tool_id=?"
+  myquery = "SELECT * FROM mydb.rental_tool WHERE tool_id=?";
   connection.query(myquery, [id], function (err, rows, fields) {
-    if (err) throw err
+    if (err) throw err;
     res.json(rows);
-  })
+  });
 
-  console.log('finished route')
+  console.log("finished route");
 });
- 
+
 //i.e. http://localhost:port/tools/searchname?name=ham
-router.get("/searchname",(req,res) => {
+router.get("/searchname", (req, res) => {
   //arguments
-  var name= req.query.name
+  var name = req.query.name;
 
-    myquery= "SELECT * FROM mydb.rental_tool WHERE LOWER(name) LIKE LOWER(CONCAT('%', ?, '%'))"
-    pool.query(myquery, function (err, rows, fields) {
-      if (err) throw err
-  
-      console.log('The solution is: ', rows)
-      res.json(rows);
-    })
+  myquery =
+    "SELECT * FROM mydb.rental_tool WHERE LOWER(name) LIKE LOWER(CONCAT('%', ?, '%'))";
+  pool.query(myquery, function (err, rows, fields) {
+    if (err) throw err;
 
-    console.log('finished route')
+    console.log("The solution is: ", rows);
+    res.json(rows);
+  });
+
+  console.log("finished route");
 });
 
 module.exports = router;
