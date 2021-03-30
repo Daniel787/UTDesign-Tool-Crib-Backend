1900881c1e0851d4b9cf2fe020624ef0de4767f5
sudhijag <37680391+sudhijag@users.noreply.github.com>
2021-03-29 19:02:43 -0500

    insert, insertExcel, still needs further testing

--- routes/student.js
+++ routes/student.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const readXlsxFile = require('read-excel-file/node');
+var toUnnamed = require('named-placeholders')();
 
 //sql connection
 var pool = require('../db.js')
@@ -43,6 +45,87 @@ router.get("/holds/detailed",(req,res) => {
   pool.query(myquery, function (err, rows, fields) {
     if (err) console.log(err)
     res.json(rows);
+  })
+});
+
+router.post("/insert", (req, res) => {
+  console.log("O3")
+   var query = toUnnamed("INSERT into mydb.Student VALUES(:net_id, :email, :utd_id, :student_hold)", {
+     net_id: req.body.net_id,
+     email: req.body.email,
+     utd_id: req.body.utd_id,
+     student_hold: req.body.student_hold
+   });
+
+   pool.query(query[0],query[1], function (err, rows, fields) {
+     if (err) console.log(err)
+ 
+     console.log('Response: ', rows)
+     res.send("finished");
+   });
+});
+
+router.post("/insertExcel", (req, res) => {
+
+  // File path.
+  readXlsxFile('Examples.xlsx').then((rows) => {
+    // `rows` is an array of rows
+    // each row being an array of cells.
+    //console.log(rows);
+
+  
+    var i,j;
+    var status=200;
+  
+    for(i=1; i< rows.length; i++){
+
+      //these are at fixed positions
+      var group_id= rows[i][0];
+      var group_name= rows[i][3];
+      var sponsor= rows[i][4];
+
+      for(j=6; j<24; j++){
+        //get name, email, id
+        var name= rows[i][j]
+        j++;
+        var id= rows[i][j]
+        j++;
+        var email= rows[i][j]
+
+        console.log("name: " + name+"    email: " + email+"     id: " + id)
+        //this check fails, but it isn't technically necessary, the insert will just fail
+        if(name == null || email == null || id == null){
+          console.log("student " + j%3 + " has a null field, skipping...")
+        }
+        else{
+          console.log("Attempting to insert a student...");
+          (async function sendquery(param) {
+            queries = []
+  
+            const pool2 = pool.promise();
+            console.log("length: ")
+              var query = toUnnamed("INSERT into mydb.Student VALUES(:net_id, :email, :utd_id, :student_hold);"
+                                    +"INSERT INTO mydb.Groups VALUES(:group_id, :group_name, :sponsor);"
+                                    +"INSERT INTO mydb.Group_Has_Student VALUES(:group_id, :net_id)", {
+                net_id: name, //for now, there is no netid in the sheet, need to ask
+                email: email,
+                utd_id: id,
+                student_hold: 0,
+                group_id: group_id,
+                group_name: group_name,
+                sponsor: sponsor
+              });
+              queries.push(pool2.query(query[0], query[1]));
+          
+        
+            //console.log("NUMQUERIES: " + queries.length);
+            //later: change one of the students to be which student and why
+            const results = await Promise.all(queries).catch(() => { console.log("One of the students failed to insert.");  status=412;});
+          })();
+        }//async
+      }//inner loop
+    }//outer loop
+    return res.status(status).send("done with route");
   })
 });
 
