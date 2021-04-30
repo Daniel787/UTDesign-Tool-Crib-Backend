3e6951f3d4aa97520b5cbe5d02dfd944891f860b
Jacob Darin Glenny <jdg180001@utdallas.edu>
2021-04-30 16:02:32 -0500

    dont show negative tools on main get call

--- routes/tools.js
+++ routes/tools.js
@@ -24,7 +24,7 @@ function validate(id, name) {
 //doesnt show deleted tools
 router.get("/", (req, res) => {
   myquery =
-     "SELECT * FROM mydb.tool_status;"
+     "SELECT * FROM mydb.tool_status WHERE tool_status.tool_id > 0;"
   pool.query(myquery, function (err, rows, fields) {
     if (err) console.log(err);
     res.json(rows);
